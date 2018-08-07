942bc03d83dd28a175f4e70765f4cf6c97be49c2
Andrea Picco <andreapicco@Andrea-3.local>
2018-08-07 18:27:51 +0200

    Update README and correct typo in setup.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # trajectory_plotting
 Plot functionalities to use with trajalign
+
+# Installation
+
+In the downloaded folder, run
+
+	sudo python setup.py install
+
+Python version must be 3.x, or greater
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup( name = 'trajectory plotting' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
 		url = 'http://apicco.github.io/trajectory_plotting/',
-		download_url = 'https://github.com/apicco/trajectory_plotting/archive/master.zip'
+		download_url = 'https://github.com/apicco/trajectory_plotting/archive/master.zip',
 		packages = [ 'trajplot' ],
 		license = 'The software is distributed under the terms of the GNU General Public License Version 3, June 2007. Trajplot, as well as Trajalign are free softwares and come with ABSOLUTELY NO WARRANTY. You are welcome to redistribute the software. However, we appreciate is use of such software would result in citations of Picco, A., Kaksonen, M., _Precise tracking of the dynamics of multiple proteins in endocytic events_,  Methods in Cell Biology, Vol. 139, pages 51-68 (2017) http://www.sciencedirect.com/science/article/pii/S0091679X16301546'
 
