ce6c93ac6c36657ff19f0e817671179a4a5aadd4
Andrea Picco <andreapicco@Andrea-3.local>
2018-08-07 18:24:08 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup( name = 'trajectory plotting' ,
 		description = 'Utilities to plot Trajalign trajectories' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
-		url = 'http://apicco.github.io/trajplot/',
-		download_url = 'https://github.com/apicco/trajplot/archive/master.zip'
+		url = 'http://apicco.github.io/trajectory_plotting/',
+		download_url = 'https://github.com/apicco/trajectory_plotting/archive/master.zip'
 		packages = [ 'trajplot' ],
-		license = 'The software is distributed under the terms of the GNU General Public License Version 3, June 2007. Trajalign is a free software and comes with ABSOLUTELY NO WARRANTY. You are welcome to redistribute the software. However, we appreciate is use of such software would result in citations of Picco, A., Kaksonen, M., _Precise tracking of the dynamics of multiple proteins in endocytic events_,  Methods in Cell Biology, Vol. 139, pages 51-68 (2017) http://www.sciencedirect.com/science/article/pii/S0091679X16301546'
+		license = 'The software is distributed under the terms of the GNU General Public License Version 3, June 2007. Trajplot, as well as Trajalign are free softwares and come with ABSOLUTELY NO WARRANTY. You are welcome to redistribute the software. However, we appreciate is use of such software would result in citations of Picco, A., Kaksonen, M., _Precise tracking of the dynamics of multiple proteins in endocytic events_,  Methods in Cell Biology, Vol. 139, pages 51-68 (2017) http://www.sciencedirect.com/science/article/pii/S0091679X16301546'
 
 		)
