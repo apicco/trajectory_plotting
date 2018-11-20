eb499cb4014cb299322db1230670d89c83ecda16
Andrea Picco <andreapicco@Andrea-3.local>
2018-11-20 11:00:31 +0100

    add variables x0, t0, which_coord and x_scale to my_plot function

--- trajplot/plotfuns.py
+++ trajplot/plotfuns.py
@@ -1,7 +1,7 @@
 from numpy import transpose, concatenate
 from matplotlib.patches import Polygon
 
-def myplot( obj , t , what , label , col , lw = 1.5 , ls = '-' ) :
+def myplot( obj , t , what , label , col , x0 = 0 , t0 = 0 , x_scale = 1 , lw = 1.5 , ls = '-' , which_coord = 0 ) :
 
 	"""
 	myplot( obj , t , what , label , col , lw = 1.5 , ls = '-' ) : plots
@@ -20,10 +20,15 @@ def myplot( obj , t , what , label , col , lw = 1.5 , ls = '-' ) :
 
 	if x.ndim > 1 : 
 
-		#then the attribute has more than one dimention and we are interested
-		#only in the first one.
-		x = x[ 0 ]
-		x_err = x_err[ 0 ]
+		#then the attribute has more than one dimention, which means it is coord and we are interested
+		#only in which_coord.
+		x = ( x[ which_coord ] - x0 ) * x_scale
+		x_err = x_err[ which_coord ] * x_scale
+	
+	else :
+
+		x = ( x - x0 ) * x_scale
+		x_err = x_err * x_scale
 
 	lower_error_boundary =  transpose( [ t.t() , x - 1.96 * x_err ] )
 	upper_error_boundary =  transpose( [ t.t() , x + 1.96 * x_err ] )
