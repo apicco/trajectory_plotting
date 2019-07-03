1ab4934bcbd4a08cac44f742581a653cadb5ccca
Andrea Picco <andreapicco@Andrea-3.local>
2019-07-03 13:09:54 +0200

    read the alignment transformation strign to align the individual trajectories to the reference used for the DW alignment

--- trajplot/plotfuns.py
+++ trajplot/plotfuns.py
@@ -2,6 +2,7 @@ from trajalign.traj import Traj
 from numpy import transpose, concatenate
 from matplotlib.patches import Polygon
 import os
+import re
 
 def myplot( obj , t , what , label , col , x0 = 0 , t0 = 0 , x_scale = 1 , lw = 1.5 , ls = '-' , which_coord = 0 ) :
 
@@ -42,7 +43,7 @@ def myplot( obj , t , what , label , col , x0 = 0 , t0 = 0 , x_scale = 1 , lw = 
 	#plot the trajectory
 	obj.plot( t.t() - t0 , x , linewidth = lw , linestyle = ls , color = col , label = label )
 
-def plot_raw( obj , path , what , label , col = "#CDCDCD" , x0 = 0 , t0 = 0 ,  x_scale = 1 , lw = 0.5 , ls = '-' , which_coord = 0 , alpha = 1 ) :
+def plot_raw( obj , path , what , label , col = "#CDCDCD" , x0 = 0 , t0 = 0 ,  x_scale = 1 , lw = 0.5 , ls = '-' , which_coord = 0 , alpha = 1 , dw = [ 0 , 0 ] ) :
 
 	files = os.listdir( path )
 
@@ -58,7 +59,15 @@ def plot_raw( obj , path , what , label , col = "#CDCDCD" , x0 = 0 , t0 = 0 ,  x
 	
 			#then the attribute has more than one dimention, which means it is coord and we are interested
 			#only in which_coord.
-			x = ( x[ which_coord ] - x0 ) * x_scale
+			dw_floats = []
+			dw_elements = [ f for f in re.split( '\[|\]|,' , dw ) ]   
+			for e in dw_elements :
+				try :
+					dw_floats.append( float( e ) )
+				except :
+					None
+
+			x = ( x[ which_coord ] - x0 - dw_floats[ which_coord ] ) * x_scale
 			x_err = x_err[ which_coord ] * x_scale
 	
 		else :
