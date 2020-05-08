deaf1e5087ae901043a41b3d3edf2461a6ba1c79
apicco <andrea.picco@unige.ch>
2020-05-08 10:44:44 +0200

    Did the following modifications, listed in a order from the most recent to the oldest: add show_on_movie_function add ID_length to show_on_movie add the possibility to input different id_shifts for the different trajectories in the dictionary, and all realtive if checks add ID_shift in show_on_movie variables add a check for coordinates in plt.text of trajectory ID change default ms in show_on_movie define x,y, and s in plt.text, as well as color add the option to show the trajectory ID on the output of show_on_movie add the function show_on_movie to plot a list of trajectories on a movie correct error in trajectory plot color" rescale the image axes according ot the scale factor add plt.close() for the sake of speed add missing color in plot_traj remove debug marker correct frameName in savefig in trajectories_on_movie correct output saving correct cmap assignment correct input orders in plot_traj fro cmap error import tiff

--- trajplot/plotfuns.py
+++ trajplot/plotfuns.py
@@ -3,6 +3,8 @@ from trajalign.average import unified_start, unified_end
 from numpy import transpose, concatenate
 from matplotlib.patches import Polygon
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from skimage.external import tifffile as tiff
 import numpy as np
 import os
 import re
@@ -196,12 +198,12 @@ def plot_raw( obj , path , what , label , which_coord = 0 , x0 = 0 , t0 = 0 ,  x
 
 def trajectories_on_movie( movie_path , output_path , tls , cmaps , scale = np.nan , scale_unit = '' , shift = [ 0 , 0 ] , figsize = ( 10, 8 ) , movie_cmap = 'gray' , marker_size = 3 , line_width = 1 ) : # tls = trajectory_lists , cmaps = colormaps )
 	
-	def plot_traj( tt , f , cmap , scale , shift , ms , lw ) :
+	def plot_traj( tt , f , cmap , shift , ms , lw ) :
 
 		l = len( tt ) 
 		for i in range( l ) :
 
-			c = cmap( i / l )
+			c = cm.get_cmap( cmap )
 
 			t = tt [ i ]
 
@@ -212,20 +214,14 @@ def trajectories_on_movie( movie_path , output_path , tls , cmaps , scale = np.n
 				sel = [ i for i in range( len( selected_frames ) ) if selected_frames[ i ] in t.frames() ]
 				u = t.extract( sel )
 
-				if scale == scale : 
-
-					plt.plot( u.coord()[ 1 ] * scale + shift[ 0 ] , u.coord()[ 0 ] * scale - shift[ 1 ] , 'o' , color = c , markersize = ms )
-					plt.plot( u.coord()[ 1 ] * scale + shift[ 0 ] , u.coord()[ 0 ] * scale - shift[ 1 ] , '-' , color = c , linewidth = lw )
-				else : 
-
-					plt.plot( u.coord()[ 1 ] + shift[ 0 ] , u.coord()[ 0 ] - shift[ 1 ] , 'o' , color = c , markersize = ms )
-					plt.plot( u.coord()[ 1 ] + shift[ 0 ] , u.coord()[ 0 ] - shift[ 1 ] , '-' , color = c , linewidth = lw )
+				plt.plot( u.coord()[ 1 ] + shift[ 0 ] , u.coord()[ 0 ] - shift[ 1 ] , 'o' , color = c( i / l ) , markersize = ms )
+				plt.plot( u.coord()[ 1 ] + shift[ 0 ] , u.coord()[ 0 ] - shift[ 1 ] , '-' , color = c( i / l ) , linewidth = lw )
 
 	#--------------------------------------------------
 
 	if ( scale == scale ) & ( len( scale_unit ) == 0 ) :
 
-		print( 'define the unit after the application of the scaling factor ')
+		print( ' define the unit after the application of the scaling factor ')
 
 	if len( tls ) == len( cmaps ) :
 		
@@ -233,14 +229,14 @@ def trajectories_on_movie( movie_path , output_path , tls , cmaps , scale = np.n
 		
 		for f in range( len( im ) ) :
 
-			frameName = output_path + 'frame' + '%03dr' % f 
+			frameName = output_path + 'frame' + '%03d.png' % f 
 			output = plt.figure( 1 , figsize = figsize )
 
 			plt.imshow( im[ f , : , : ] , cmap = movie_cmap )
 			
 			for i in range( len( tls ) ) :
 
-				plot_traj( tls[ i ] , f , scale , shift , cmap = cmaps[ i ] , ms = marker_size , lw = line_width )
+				plot_traj( tls[ i ] , f , cmap = cmaps[ i ] , shift = shift , ms = marker_size , lw = line_width )
 
 			if not scale == scale :
 
@@ -248,11 +244,188 @@ def trajectories_on_movie( movie_path , output_path , tls , cmaps , scale = np.n
 				plt.ylabel( 'Pixel' )
 
 			else :
-				
-				plt.xlabel( scale_unit )
-				plt.ylable( scale_unit )
 
-			plt.savefig( output_path + frameName )
+				xo = [ i for i in range( 0 , im.shape[ 2 ] , 100 ) ]
+				plt.xticks( xo , [ scale * i for i in xo ] )
+				yo = [ i for i in range( 0 , im.shape[ 1 ] , 100 ) ]
+				plt.yticks( yo , [ scale * i for i in yo ] )
+				plt.xlabel( scale_unit )
+				plt.ylabel( scale_unit )
+
+			print( frameName ) 
+			plt.savefig( frameName )
+			plt.close()
+
 	else :
 
 		print( 'Specify as many cmaps as there are trajectory lists' )
+
+
+def show_on_movie( tt , im , path_output , col , mstyl , figsize = ( 8 , 7 ) , ms = 20 , lw = 3 , alpha = 0.5 , ID = False , ID_shift = {} , ID_length = np.nan ) :
+
+	def plot_traj( t , f , c , mstyl , ms , lw , a , ID , ID_shift = [ 4 , 4 ] , ID_length = np.nan ) :
+	
+		shift = 0# correct PT shift
+	
+		if ( f + 1 >= t.frames()[ 0 ] ) & ( f + 1 <= t.frames()[ -1 ] ) :
+
+			#selected frames
+			sel = [ i for i in range( len( t ) ) if t.frames()[ i ] <= f + 1 ]
+	
+			u = t.extract( sel )
+
+			plt.plot( u.coord()[ 1 ][ -1 ] + shift , u.coord()[ 0 ][ -1 ] + shift , mstyl , color = c , markersize = ms , markerfacecolor = 'none' , alpha = a )
+			plt.plot( u.coord()[ 1 ] + shift, u.coord()[ 0 ] + shift , '-' , color = c , linewidth = lw , alpha = a )
+
+			if ( ID & ( u.coord()[ 1 ][ -1 ] == u.coord()[ 1 ][ -1 ] ) & ( u.coord()[ 0 ][ -1 ] == u.coord()[ 0 ][ -1 ] ) ) :
+
+				ID_text = t.annotations()[ 'file' ]
+
+				if ID_length == ID_length :
+					
+					ID_text = ID_text[ - ( 4 + ID_length ) : -4 ]
+
+				plt.text( x = u.coord()[ 1 ][ -1 ] + shift + ID_shift[ 0 ] , y = u.coord()[ 0 ][ -1 ] + shift + ID_shift[ 1 ] , s = ID_text , color = c )
+
+	# -------------------------
+
+	l = len( tt )
+
+	if ID :
+		
+		if ( len( ID_shift ) > 0 ) & ( isinstance( ID_shift , dict ) ) :
+		
+			if  l != len( ID_shift ) :
+		
+				raise TypeError( 'the ID_shift dictionary and the tt dictionary  must have the same length ' )
+		else : 
+
+			if 2 != len( ID_shift) :
+				
+				raise TypeError( 'the ID_shift list  must have only two values, one for the shift along x, the other for the shift along y ' )
+
+	if l != len( col ) :
+
+		raise TypeError( 'the tt dictionary and the list col must have the same length ' )
+
+	if not os.path.exists( path_output ) :
+
+		os.makedirs( path_output )
+
+	for f in range( 0 , len( im ) ) :
+
+		frameName = path_output + '/' + 'frame' + '%04d' % f
+
+		plt.figure( 1 , figsize = figsize )
+		plt.axes().set_aspect( 'equal' )
+
+		plt.imshow( im[ f , : , : ] , cmap = 'gray' )
+		
+		for i in range( l ) :
+		
+			#load the trajectory list
+			k = list( tt.keys() )[ i ]
+			tl = tt[ k ]
+
+			for t in tl :
+
+				#define the correct ID shift if there are multiple trajectory lists and plot
+				if ID :
+
+					if isinstance( ID_shift , dict )  :
+						ids = ID_shift[ k ]
+					else :
+						ids = ID_shift
+				
+					plot_traj( t , f , c = col[ i ] , mstyl = mstyl[ i ] , ms = ms , lw = lw , a = alpha , ID = ID , ID_shift = ids , ID_length = ID_length ) 
+				
+				else :
+
+					plot_traj( t , f , c = col[ i ] , mstyl = mstyl[ i ] , ms = ms , lw = lw , a = alpha ) 
+
+		plt.xlabel( 'Pixel' )
+		plt.ylabel( 'Pixel' )
+		plt.savefig( frameName )
+		plt.close()
+		
+
+def show_selected_trajectories( tt , movie_path , output_path , dataset_path = '' , n = np.nan , r = 10 , n_col = 10 ) :
+	
+	"""
+	selected_trajectories( tt , movie_path , n = 3 , r = 10 ) show snapshots of the spots 
+	from which the trajectories in the list tt were derived. The snapshots are generated from the 
+	trajectory movies in movie_path using n frames equally distributed across the trajectory lifetime. 
+	'r' determines the radius of the snapshot defined which will cover a region [ c - r , c + r ], where c is the centroi position, all in pixels.
+	The trajectories are mapped to the right movie finding the trajectory in the traj.txt file, unless 
+	the movie name is in the trajectory annotations already, using assign_datasetID
+	"""
+
+	for t in tt : 
+
+		print( t.annotations() )
+
+		# load the movie name, it could be that the trajectory as already 
+		# the annotation 'dataset', in which case the dataset_path is not 
+		# needed (default) 
+		if t.annotations()[ 'dataset' ] :
+			
+			movie_name = t.annotations()[ 'dataset' ][ 5 : -4 ] + '.tif'
+		
+		elif len( dataset_path ) :
+			
+			t.assign_datasetID( dataset_path )
+			movie_name = t.annotations()[ 'dataset' ][ 5 : -4 ] + '.tif'
+
+		else :
+
+			raise AttributeError( 'trajectory has no annotation dataset, and there is no dataset_path' )
+
+		im = tiff.imread( movie_path + '/' + movie_name )
+
+		# exctract the trajectory name, if it has a file exension that's left out
+		t_name = t.annotations()[ 'file' ].split( '.' )[ 0 ]
+
+		# if n is not defiend, n is the length of the trajectory
+		if n != n : 
+			n = len( t )
+		# and n cannot be one otherwise n-1 denominators will diverge. 
+		# Also 1 example, or a 1 element trajectory are meaningless
+		if n == 1 : 
+			raise AttributeError( 'n must be greather than 1, or the trajectory must have more than one datapoint' )
+
+		# define frames and coordinates without nan
+		f = [ t.frames( i ) for i in range( len( t ) ) if t.coord( i )[ 0 ] == t.coord( i )[ 0 ] ]
+		x = [ t.coord( i )[ 0 ] for i in range( len( t ) ) if t.coord( i )[ 0 ] == t.coord( i )[ 0 ] ] 
+		y = [ t.coord( i )[ 1 ] for i in range( len( t ) ) if t.coord( i )[ 0 ] == t.coord( i )[ 0 ]  ] 
+		
+		# define what is the delta-frame interval needed to show the desired number 
+		# of equally spaced frames that to show the spot
+		df = int( np.ceil( len( f ) / ( n ) ) )
+
+		# select the frame numbers to plot
+		sel = [ i for i in range( 0 , len( f ) , df ) ]
+
+		# select the frames and centroid coordinates
+		sel_f = [ f[ i ] for i in sel ]
+		sel_x = [ int( np.round( x[ i ] ) ) for i in sel ]
+		sel_y = [ int( np.round( y[ i ] ) ) for i in sel ]
+
+		# define the number of rows in the array of subplots
+		n_row = int( np.ceil( n / n_col ) )
+
+		# plot the figure
+		plt.figure( figsize = ( n_col * 5 , n_row * 4 ) )
+
+		for i in range( len( sel_f ) ) :
+			
+			plt.subplot( n_row , n_col , i + 1 )
+			plt.title( 'frame ' + str( sel_f[ i ] ) )
+			
+			# centroid
+			sim =  im[ sel_f[ i ] , - r + sel_x[ i ] : sel_x[ i ] + r , - r + sel_y[ i ] : sel_y[ i ] + r ]
+			plt.imshow( sim )
+			i = i + 1
+
+		plt.savefig( output_path + '/' + t_name + '.pdf' )		
+
+
