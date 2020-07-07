909b353deb0aca22b837ed41b698d7c7532ffec1
apicco <andrea.picco@unige.ch>
2020-07-07 13:32:54 +0200

    attempt to define axesimage'

--- trajplot/autofuns.py
+++ trajplot/autofuns.py
@@ -1,18 +1,94 @@
 import os
 import tkinter as tk
 import numpy as np
-from  matplotlib.colors import Normalize as norm
+from matplotlib.colors import Normalize as norm
+from matplotlib.figure import Figure 
+
 from skimage.external import tifffile as tiff
 from trajalign.traj import Traj
 from trajalign.average import load_directory
 
 # to plot image frames
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-# Implement the default Matplotlib key bindings.
-from matplotlib.figure import Figure
-from matplotlib.backend_bases import key_press_handler
 
-def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7 , cmap = 'gray' , path_output = './' , marker = 's' , markersize = 25 , offset = ( 0 , 0 ) ) :
+# frames before and after as input parameters.
+
+class Repr( Figure ) :
+
+	def __init__( self , trajlist , j , r , im ) :
+
+		self.j = j
+		self.tlist = trajlist
+		self.trajectory = self.tlist[ self.j ]
+		self.image = im # image
+		self.r = r # zoom radius
+		self.fmin = np.nanmin( self.trajectory.frames() )
+		self.fmax = np.nanmax( self.trajectory.frames() )
+		self.f = self.fmin # frame
+		self.figure = Figure( figsize = ( 5 , 4 ) , dpi = 100 )
+		self.ax = self.figure.add_subplot( 111 )
+
+		print( self.lims() )
+		self.aximg = self.ax.imshow( 
+				[ self.f , self.lims()[ 'ylims'][ 0 ] : self.lims()[ 'ylims'][ 1 ] , self.lims()[ 'xlims' ][ 0 ] : self.lims()[ 'xlims' ][ 1 ] ] ,  
+				norm = norm( vmin = np.amin( self.image ) , vmax = np.amax( self.image ) ) , # intensity normalization
+				cmap = cmap
+			)
+
+																# will be used to update the rendering
+
+	def df( self , d ) : # increment frames by d
+
+		c = [ np.nan , np.nan ] # initiate a nan centroid coordinate
+								# to enter the while loop
+		
+		old_frame = self._f		# store the old frame, in case there are
+								# only nan until the end of the trajectory
+								# file
+		while c[ 0 ] != c[ 0 ] :
+					
+			if ( ( self._f + d >= self._fmin ) & ( self._f + d <= self._fmax ) ) :
+		
+				self._f = self._f + d
+				c = self.centroid()
+
+			else :
+				
+				self._f = old_fram
+				break
+
+	def zoom( self , r ) :
+
+		self._r = self._r + r 
+
+	def centroid( self ) :
+
+		return [ self.trajectory.coord()[ 0 ][ self.f - self.fmin ] , self.trajectory.coord()[ 1 ][ self.f - self.fmin ] ] #i-th element in centrod coord is f - fmin
+
+	def lims( self ) :
+
+		return dict( xlims = [ max( 0 , int( - self.r + self.centroid()[0] ) ) , min( int( self.centroid()[0] + self.r ) , len( self.image[ 0 , 1 , : ] ) - 1 ) ] , ylims = [ max( 0 , int( -self.r + self.centroid()[1] ) ) , min( int( self.centroid()[1] + self.r ) , len( self.image[ 0 , : , 1 ] ) - 1 ) ] )
+
+	def render( self , cmap ) : # rende the image 
+
+		self.aximg
+		self.ax.set_xlabel( "Pixels" )
+		self.ax.set_ylabel( "Pixels" )
+		self.ax.set_title( self.trajectory.annotations()[ 'file' ] + ' ' + '\n' + \
+				'trajectory ' + str( self.j + 1 ) + '/' + str( len( self.tlist ) ) + '; ' + 'frame ' + str( self.f - self.fmin ) + '/' + str( self.fmax - self.fmin ) + '; ' + \
+				r'$r=$' + str( self.r ) )
+	print( 'end render' )
+
+	def update( self ) :
+		
+		print( 'update' ) 
+		print( self.f )
+		print( self.lims() )
+		img = self.image[ self.f , self.lims()[ 'ylims'][ 0 ] : self.lims()[ 'ylims'][ 1 ] , self.lims()[ 'xlims' ][ 0 ] : self.lims()[ 'xlims' ][ 1 ] ] ,  # image 
+		print( img ) 
+		self.aximg.set_array( img )
+
+def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7 , cmap = 'gray' , path_output = './' , marker = 's' , markersize = 25 , offset = ( 0 , 0 ) , anticipate = 0 ) :
 	"""
 	icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7 , cmap = 'gray' ) :
 		load the trajectories in path_trajectories 
@@ -31,88 +107,39 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 	To do not bias the experimenter the trajectory position within the cell is not shown (compatibly with "r").
 	"""
 
-	def GUI_plot( tt , df , ms = markersize , marker = marker , offset = offset ) : # show the frame
-
-		# select the trajctory
-		t = tt[ v[ 'j' ] ]
-		t_name = t.annotations()[ 'file' ]
+	def LoadMovie( path_movies , path_movie , t ) : 
 		
-		# identify which centroid coordinate to associate with the frame
-		# start with nan centroid, centroid coordines must not be nan, therefore
-		# search for the first not nan and increment i "frame" accordingly
-		c = [ np.nan , np.nan ]
-		v_frame_old = v[ 'frame' ]  # used to stall the movie in case v[ "frame" ] reaches
-									# frame_min, frame_max, or movie length
-		while c[ 0 ] != c[ 0 ] :
-
-			v[ "frame" ] = v[ "frame" ] + df
-			i = v[ "frame" ] - v[ "frame_min" ] # trajectory element
-			if ( 
-					( v[ "frame" ] >= v[ "frame_min" ] ) & 
-					(  v[ "frame" ] <= v[ "frame_max" ] ) & 
-					( v[ "frame" ] < len( v[ 'image' ] ) ) 
-				) : # check that we do not exceed frame_min, frame_max, and movie length
-				
-					c = [ t.coord()[ 0 ][ i ] , t.coord()[ 1 ][ i ] ] # centroid coordinate
-
-			else :
-
-				v[ "frame" ] = v_frame_old 
-				i = v[ "frame" ] - v[ "frame_min" ] # trajectory element
-				c = [ t.coord()[ 0 ][ i ] , t.coord()[ 1 ][ i ] ] # centroid coordinate
-				break
-
-		#ax.clear()
-
-		# Plot image. Note: +1 in centroid positions is to center the spot in the quadrant. 
-		# I suspect probem between PT and python nomenclatures 
-		# (one starts at 1 the other at 0) 
+		if path_movies : 
 	
-		xlims = [ max( 0 , int( -v[ "r" ] + c[0] ) ) ,
-			min( int( c[0] + v[ "r" ] ) , len( v[ 'image'][ 0 , 1 , : ] ) - 1 ) ]
-		ylims = [ max( 0 , int( -v[ "r" ] + c[1] ) ) ,
-			min( int( c[1] + v[ "r" ] ) , len( v[ 'image'][ 0 , : , 1 ] ) - 1 ) ]
+			movie_name = t.annotations()[ 'dataset' ][ 5 : -4 ] + '.tiff' 
+			return tiff.imread( path_movies + '/' + movie_name )
 
-		IMG = v[ 'image' ][ int( v[ "frame" ] ) , ylims[0] : ylims[1] , xlims[0] : xlims[1] ] 
+		else : 
+
+			return tiff.imread( path_movie )
+
+	def LeftKey( event , frame , increment = -1 ) :
+		
+		frame.df( increment ) #frame increment to the left in case of nan
+		frame.update()
+
+			
+	def RightKey( event , frame , incrememnt = 1 ) :
+
+		frame.df( increment ) 
+		frame.update()
+
+	def ZoomOut( event , frame , increment = 1 ) :
+		
+		frame.zoom( increment ) 
+
+	def ZoomIn( event , frame , increment = -1 ) :
+		
+		frame.zoom( increment ) 
+			
+	def UpKey( event , frame , path , f ) :
 	
-		render.set_array( IMG )
-		new_centroid = [ c[0] - xlims[ 0 ] + offset[ 0 ] ,  c[1] - ylims[ 0 ] + offset[ 1 ] ]
-		centroid_plot[0].set_data( new_centroid )
-
-		ax.set_xlabel( "Pixels" )
-		ax.set_ylabel( "Pixels" )
-		ax.set_title( t_name + ' ' + '\n' + \
-				'trajectory ' + str( v[ 'j' ] + 1 ) + '/' + str( len( tt ) ) + '; ' + 'frame ' + str( i ) + '/' + str( v[ "frame_max" ] - v[ "frame_min" ] ) + '; ' + \
-				r'$r=$' + str( v[ "r" ] ) )
-		canvas.draw()
-
-	def LeftKey( event , tt ) :
-		
-		df = -1 #frame increment to the left in case of nan 	
-		GUI_plot( tt , df ) 
-			
-	def RightKey( event , tt ) :
-
-		df = 1  #frame increment to the right in case of nan	
-		GUI_plot( tt , df )
-
-	def ZoomOut( event , tt ) :
-		
-		v[ "r" ] = v[ "r" ] + 1
-		df = 0
-		GUI_plot( tt , df ) 
-
-	def ZoomIn( event , tt ) :
-		
-		v[ "r" ] = v[ "r" ] - 1
-		df = 0
-		GUI_plot( tt , df ) 
-			
-	def UpKey( event , tt , path , f ) :
-		
-		# select the trajctory
-		t = tt[ v[ 'j' ] ]
-
+		t = frame.trajectory()
 		t.annotations()[ "icheck_stamp" ] = "Selected" 
 		t.save( path + '/' + t.annotations()[ "file" ] )
 
@@ -120,23 +147,23 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 		f.write( s_log + '\n' )
 		print( s_log )
 			
-		if v[ 'j' ] == ( len( tt ) - 1 ) :
-
-			f.close()
-			SpotWindow.destroy()
-
-		else :
-			
-			# move to the next trajectory
-			v[ 'j' ] = v[ 'j' ] + 1 
-			Update_v( tt )
-			
-			GUI_plot( tt , 1 )
+#		if j == ( len( tt ) - 1 ) :
+#
+#			f.close()
+#			SpotWindow.destroy()
+#
+#		else :
+#			
+#			# move to the next trajectory
+#			v[ 'j' ] = v[ 'j' ] + 1 
+#			Update_v( tt )
+#			
+#			GUI_plot( tt , 1 )
 		
 	def DownKey( event , tt , path , f ) :
 		
 		# select the trajctory
-		t = tt[ v[ 'j' ] ]
+		t = frame.trajectory()
 		
 		t.annotations()[ "icheck_stamp" ] = "Rejected" 
 		t.save( path + '/' + t.annotations()[ "file" ] )
@@ -145,67 +172,32 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 		f.write( s_log + '\n' )
 		print( s_log )
 	
-		if v[ 'j' ] == ( len( tt ) - 1 ) :
-			f.close()
-			SpotWindow.destroy()
-		else :
-			# move to the next trajectory
-			v[ 'j' ] = v[ 'j' ] + 1 
-			Update_v( tt )
-			GUI_plot( tt , 1 )
+#		if v[ 'j' ] == ( len( tt ) - 1 ) :
+#			f.close()
+#			SpotWindow.destroy()
+#		else :
+#			# move to the next trajectory
+#			v[ 'j' ] = v[ 'j' ] + 1 
+#			Update_v( tt )
+#			GUI_plot( tt , 1 )
 
 	def ExitHeader( event ) :
 
 		HeaderWindow.destroy()
 
-	def Update_v( tt ) :
-	
-		# select the trajctory
-		t = tt[ v[ 'j' ] ]
-		
-		# load the rigth movie
-		movie_name = t.annotations()[ 'dataset' ][ 5 : -4 ] + '.tif'
-
-		# if the movie is already loaded in v, don't load it.
-		if movie_name != v[ 'movie_name' ] : 
-
-			if v[ "path_movies" ] :
-				
-				im = tiff.imread( v[ 'path_movies' ] + '/' + movie_name )
-			
-			elif v[ "path_movie" ] : 
-	
-				im = tiff.imread( v[ 'path_movie' ] )
-	
-			else :
-	
-				raise AttributeError( 'Verify that path_movie, to a single movie tif file, or path_movies, to a folder containing one or more movie tif files, are correct' )
-	
-			v[ 'image' ] = im
-
-		v[ "frame_min" ] = np.nanmin( tt[ v[ 'j' ] ].frames() )
-		v[ "frame_max" ] = np.nanmax( tt[ v[ 'j' ] ].frames() ) 
-		v[ "frame" ] = v[ "frame_min" ] - 1 # the -1 because Update_v is used before 
-											# plotting the first frame of a spot.
-											# Plotting is done with GUI_plot that does 
-											# df increments of magnitude +/- 1. As the 
-											# first frame can have nan coordinates, it 
-											# is important to use GUI_plot( tt , 1 ) instead
-											# of GUI_plot( tt , -1 ). Hence the -1 here.
-
-	def BackKey( event , tt , path_sel , path_rej ) :
-
-		if v[ 'j' ] > 0 :
-
-			v[ 'j' ] = v[ 'j' ] - 1
-			try : 
-				os.remove( path_sel + '/' + tt[ v[ 'j' ] ].annotations()[ 'file' ] )
-			except :
-				os.remove( path_rej + '/' + tt[ v[ 'j' ] ].annotations()[ 'file' ] )
-			print( tt[ v[ 'j' ] ].annotations()[ 'file' ] + '\t-undo-' )
-			
-			Update_v( tt )
-			GUI_plot( tt , 1 )
+#	def BackKey( event , tt , path_sel , path_rej ) :
+#
+#		if v[ 'j' ] > 0 :
+#
+#			v[ 'j' ] = v[ 'j' ] - 1
+#			try : 
+#				os.remove( path_sel + '/' + tt[ v[ 'j' ] ].annotations()[ 'file' ] )
+#			except :
+#				os.remove( path_rej + '/' + tt[ v[ 'j' ] ].annotations()[ 'file' ] )
+#			print( tt[ v[ 'j' ] ].annotations()[ 'file' ] + '\t-undo-' )
+#			
+#			Update_v( tt )
+#			GUI_plot( tt , 1 )
 		
 	HeaderWindow = tk.Tk() # create a Tkinter window
 	HeaderWindow.wm_title( 'icheck' )
@@ -224,23 +216,6 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 	loading = "LOADING trajectories ASSIGNING their dataset ID..."
 	loaded = "trajectories are loaded and assigned to their dataset ID.\n-> PRESS <space> TO CONTINUE <-"
 	
-	# v is a dict of variables containing all the relevant variables that need to be passed to the 
-	# binding functions in tk. You should think of i as a list of pointers. i includes
-	# "frame" : the current frame shown in the GUI
-	# "frame_min" : the min frame value that can be shown, for that given trajectory. When a trajectory is 
-	#				shown for the first time then i["frame"] = i["frame_min"]
-	# "frame_max" : the max frame value that can be shown, for that given trajectory
-	# "j" : the trajectory id in the trajectory list
-	# "r" : the crop radius
-
-	if path_movie : 
-	
-		v = dict( frame = np.nan , frame_min = np.nan , frame_max = np.nan , j = 0 , r = r , cmap = cmap , path_movies = '' , path_movie = path_movie , movie_name = '' , image = [] )
-
-	else :
-		
-		v = dict( frame = np.nan , frame_min = np.nan , frame_max = np.nan , j = 0 , r = r , cmap = cmap , path_movies = path_movies , path_movie = '' , movie_name = '' , image = [] )
-
 	# --------------------------------------------------- #
 
 	# Spot selection can be done multiple times to asses its 
@@ -274,6 +249,7 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 		print( '...dataset ID assigned' )
 	
 	# --------------------------------------------------- #
+	
 	l = tk.Label( HeaderWindow , text = header + loaded )
 	l.pack()
 	
@@ -285,75 +261,21 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 	SpotWindow = tk.Tk()
 	SpotWindow.wm_title( 'icheck' )
 
-	Update_v( tt )
+	frm = Repr( tt , 0 , r , LoadMovie( path_movies , path_movie , tt[ 0 ] ) )
 
-	fig = Figure( figsize = (  5 , 4 ) , dpi = 100 )
-	ax = fig.add_subplot( 111 )
-	canvas = FigureCanvasTkAgg( fig , master = SpotWindow )  # A tk.DrawingArea.
-
-	# GUIplot equivalent
-	df = 1 
-
-	# select the trajctory
-	t = tt[ v[ 'j' ] ]
-	t_name = t.annotations()[ 'file' ]
+	canvas = FigureCanvasTkAgg( frm.figure , master = SpotWindow )  # A tk.DrawingArea.
 	
-	# identify which centroid coordinate to associate with the frame
-	# start with nan centroid, centroid coordines must not be nan, therefore
-	# search for the first not nan and increment i "frame" accordingly
-	c = [ np.nan , np.nan ]
-	v_frame_old = v[ 'frame' ]  # used to stall the movie in case v[ "frame" ] reaches
-								# frame_min, frame_max, or movie length
-	while c[ 0 ] != c[ 0 ] :
-
-		v[ "frame" ] = v[ "frame" ] + df
-		i = v[ "frame" ] - v[ "frame_min" ] # trajectory element
-		if ( 
-				( v[ "frame" ] >= v[ "frame_min" ] ) & 
-				(  v[ "frame" ] <= v[ "frame_max" ] ) & 
-				( v[ "frame" ] < len( v[ 'image' ] ) ) 
-			) : # check that we do not exceed frame_min, frame_max, and movie length
-			
-				c = [ t.coord()[ 0 ][ i ] , t.coord()[ 1 ][ i ] ] # centroid coordinate
-
-		else :
-
-			v[ "frame" ] = v_frame_old 
-			i = v[ "frame" ] - v[ "frame_min" ] # trajectory element
-			c = [ t.coord()[ 0 ][ i ] , t.coord()[ 1 ][ i ] ] # centroid coordinate
-			break
-
-	# Plot image. Note: +1 in centroid positions is to center the spot in the quadrant. 
-	# I suspect probem between PT and python nomenclatures 
-	# (one starts at 1 the other at 0) 
-
-	xlims = [ max( 0 , int( -v[ "r" ] + c[0] ) ) ,
-		min( int( c[0] + v[ "r" ] ) , len( v[ 'image'][ 0 , 1 , : ] ) - 1 ) ]
-	ylims = [ max( 0 , int( -v[ "r" ] + c[1] ) ) ,
-		min( int( c[1] + v[ "r" ] ) , len( v[ 'image'][ 0 , : , 1 ] ) - 1 ) ]
-
-	IMG = v[ 'image' ][ int( v[ "frame" ] ) , ylims[0] : ylims[1] , xlims[0] : xlims[1] ] 
-
-	render = ax.imshow( IMG , cmap = v[ 'cmap' ]  , norm = norm(  vmin = np.amin( v[ 'image' ] ) , vmax = np.amax( v[ 'image' ] ) ) )
-
-	centroid_plot = ax.plot( c[0] - xlims[ 0 ] + offset[ 0 ] ,  c[1] - ylims[ 0 ] + offset[ 1 ] , color = 'red' , marker = marker , mew = 0.5 , ms = markersize , fillstyle = 'none' , )
-	ax.set_xlabel( "Pixels" )
-	ax.set_ylabel( "Pixels" )
-	ax.set_title( t_name + ' ' + '\n' + \
-			'trajectory ' + str( v[ 'j' ] + 1 ) + '/' + str( len( tt ) ) + '; ' + 'frame ' + str( i ) + '/' + str( v[ "frame_max" ] - v[ "frame_min" ] ) + '; ' + \
-			r'$r=$' + str( v[ "r" ] ) )
+	frm.render( cmap )
+	
 	canvas.draw()
-
-	# end  GUIplot equivalent	
-
 	canvas.get_tk_widget().pack( side = tk . TOP , fill=tk.BOTH , expand = 1 )
-
-	SpotWindow.bind( "<Left>" , lambda event , tt = tt : LeftKey( event , tt ) )
-	SpotWindow.bind( "<Right>" , lambda event , tt = tt : RightKey( event , tt ) )
-	SpotWindow.bind( "<Up>" , lambda event , tt = tt , path = ps , f = f : UpKey( event , tt , path , f ) )
-	SpotWindow.bind( "<Down>" , lambda event , tt = tt , path = pr , f = f : DownKey( event , tt , path , f ) )
-	SpotWindow.bind( "<BackSpace>" , lambda event , tt = tt , path_sel = ps , path_rej = pr : BackKey( event , tt , path_sel , path_rej ) )
-	SpotWindow.bind( "-" , lambda event , tt = tt : ZoomOut( event , tt ) )
-	SpotWindow.bind( "+" , lambda event , tt = tt : ZoomIn( event , tt ) )
-	
+#
+	SpotWindow.bind( "<Left>" , lambda event , frame = frm : LeftKey( event , frame ) )
+	SpotWindow.bind( "<Right>" , lambda event , frame = frm : RightKey( event , frame ) )
+#	#SpotWindow.bind( "<Up>" , lambda event , frame = frm , path = ps , f = f : UpKey( event , tt , path , f ) )
+#	#SpotWindow.bind( "<Down>" , lambda event , tt = tt , path = pr , f = f : DownKey( event , tt , path , f ) )
+##	SpotWindow.bind( "<BackSpace>" , lambda event , tt = tt , path_sel = ps , path_rej = pr : BackKey( event , tt , path_sel , path_rej ) )
+#	SpotWindow.bind( "-" , lambda event , frame = frm : ZoomOut( event , frame ) )
+#	SpotWindow.bind( "+" , lambda event , frame = frm : ZoomIn( event , frame ) )
+#	
 	SpotWindow.mainloop()
