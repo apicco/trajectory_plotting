13de35eb97c44b6b9ea780926159c4af6cdb5428
apicco <andrea.picco@unige.ch>
2020-07-09 11:22:47 +0200

    fix fast frame scrolling and zooming

--- trajplot/autofuns.py
+++ trajplot/autofuns.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import numpy as np
 from matplotlib.colors import Normalize as norm
 from matplotlib.figure import Figure 
-
+import matplotlib.pyplot as plt
 from skimage.external import tifffile as tiff
 from trajalign.traj import Traj
 from trajalign.average import load_directory
@@ -15,45 +15,48 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class Repr( Figure ) :
 
-	def __init__( self , trajlist , j , r , im ) :
+	def __init__( self , trajlist , j , r , im , master , cmap ) :
 		
-		# trajectory
+		# trajectory data
 		self.j = j
 		self.tlist = trajlist
 		self.trajectory = self.tlist[ self.j ]
 		self.fmin = np.nanmin( self.trajectory.frames() )
 		self.fmax = np.nanmax( self.trajectory.frames() )
 		self.f = self.fmin # frame
-		self.figure = Figure( figsize = ( 5 , 4 ) , dpi = 100 )
-		self.ax = self.figure.add_subplot( 111 )
 
-		# image 
+		# image data 
 		self.image = im # image
 		self.r = r # zoom radius
+
+		# artist's canvas
+		self.cmap = cmap
+		self.figure = plt.figure( figsize = ( 5 , 4 ) , dpi = 100 )
+		self.canvas = FigureCanvasTkAgg( self.figure , master = master ) 
 
 	def df( self , d ) : # increment frames by d
 
 		c = [ np.nan , np.nan ] # initiate a nan centroid coordinate
 								# to enter the while loop
 		
-		old_frame = self._f		# store the old frame, in case there are
+		old_frame = self.f		# store the old frame, in case there are
 								# only nan until the end of the trajectory
 								# file
 		while c[ 0 ] != c[ 0 ] :
 					
-			if ( ( self._f + d >= self._fmin ) & ( self._f + d <= self._fmax ) ) :
+			if ( ( self.f + d >= self.fmin ) & ( self.f + d <= self.fmax ) ) :
 		
-				self._f = self._f + d
+				self.f = self.f + d
 				c = self.centroid()
 
 			else :
 				
-				self._f = old_frame
+				self.f = old_frame
 				break
 
 	def zoom( self , r ) :
 
-		self._r = self._r + r 
+		self.r = self.r + r 
 
 	def centroid( self ) :
 
@@ -63,27 +66,42 @@ class Repr( Figure ) :
 
 		return dict( xlims = [ max( 0 , int( - self.r + self.centroid()[0] ) ) , min( int( self.centroid()[0] + self.r ) , len( self.image[ 0 , 1 , : ] ) - 1 ) ] , ylims = [ max( 0 , int( -self.r + self.centroid()[1] ) ) , min( int( self.centroid()[1] + self.r ) , len( self.image[ 0 , : , 1 ] ) - 1 ) ] )
 
+	def clear( self ) :
 		
-	def render( self , cmap ) : # rende the image 
+		plt.clf()
+		
+	def render( self ) : # rende the image 
 
-		aximg = self.ax.imshow( 
-				[ self.f , self.lims()[ 'ylims'][ 0 ] : self.lims()[ 'ylims'][ 1 ] , self.lims()[ 'xlims' ][ 0 ] : self.lims()[ 'xlims' ][ 1 ] ] ,  
+		# load the region of interest within the image
+		img = self.image[ int( self.f ) , self.lims()[ 'ylims'][ 0 ] : self.lims()[ 'ylims'][ 1 ] , self.lims()[ 'xlims' ][ 0 ] : self.lims()[ 'xlims' ][ 1 ] ]
+	
+		# show the image, aximg is outputed to refresh its content
+		# in order to navigate faster within the frames
+		aximg = plt.imshow( 
+				img ,
 				norm = norm( vmin = np.amin( self.image ) , vmax = np.amax( self.image ) ) , # intensity normalization
-				cmap = cmap
+				cmap = self.cmap
 				)											
-
-		self.ax.set_xlabel( "Pixels" )
-		self.ax.set_ylabel( "Pixels" )
-		self.ax.set_title( self.trajectory.annotations()[ 'file' ] + ' ' + '\n' + \
+		
+		# graphical parameters
+		plt.xlabel( "Pixels" )
+		plt.ylabel( "Pixels" )
+		plt.title( self.trajectory.annotations()[ 'file' ] + ' ' + '\n' + \
 				'trajectory ' + str( self.j + 1 ) + '/' + str( len( self.tlist ) ) + '; ' + 'frame ' + str( self.f - self.fmin ) + '/' + str( self.fmax - self.fmin ) + '; ' + \
 				r'$r=$' + str( self.r ) )
-
+		
+		self.canvas.draw()
+		
 		return aximg
 
 	def update( self , aximg ) :
-		
-		img = self.image[ self.f , self.lims()[ 'ylims'][ 0 ] : self.lims()[ 'ylims'][ 1 ] , self.lims()[ 'xlims' ][ 0 ] : self.lims()[ 'xlims' ][ 1 ] ] ,  # image 
+	
+		img = self.image[ int( self.f ) , self.lims()[ 'ylims'][ 0 ] : self.lims()[ 'ylims'][ 1 ] , self.lims()[ 'xlims' ][ 0 ] : self.lims()[ 'xlims' ][ 1 ] ]  # image 
 		aximg.set_array( img )
+		plt.title( self.trajectory.annotations()[ 'file' ] + ' ' + '\n' + \
+				'trajectory ' + str( self.j + 1 ) + '/' + str( len( self.tlist ) ) + '; ' + 'frame ' + str( self.f - self.fmin ) + '/' + str( self.fmax - self.fmin ) + '; ' + \
+				r'$r=$' + str( self.r ) )
+		self.canvas.draw()
 
 def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7 , cmap = 'gray' , path_output = './' , marker = 's' , markersize = 25 , offset = ( 0 , 0 ) , anticipate = 0 ) :
 	"""
@@ -118,21 +136,29 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 	def LeftKey( event , frame , increment , aximg ) :
 		
 		frame.df( increment ) #frame increment to the left in case of nan
-		frame.update( aximg )
+		frame.update( aximg[0] )
 
-			
 	def RightKey( event , frame , increment , aximg ) :
 
 		frame.df( increment ) 
-		frame.update( aximg )
-
-	def ZoomOut( event , frame , increment = 1 ) :
+		frame.update( aximg[0] )
 		
-		frame.zoom( increment ) 
+		frm.canvas.get_tk_widget().pack( side = tk . TOP , fill=tk.BOTH , expand = 1 )
 
-	def ZoomIn( event , frame , increment = -1 ) :
-		
+	def ZoomOut( event , frame , increment , aximg ) :
+	 
+		frame.clear()
+
 		frame.zoom( increment ) 
+		aximg[0] = frame.render()
+	
+	def ZoomIn( event , frame , increment , aximg ) :
+	
+		frame.clear()
+		
+		frame.zoom( increment )
+		
+		aximg[0] = frame.render()
 			
 	def UpKey( event , frame , path , f ) :
 	
@@ -258,21 +284,18 @@ def icheck( tt , path_movies = '' , path_datasets = '' , path_movie = '' , r = 7
 	SpotWindow = tk.Tk()
 	SpotWindow.wm_title( 'icheck' )
 
-	frm = Repr( tt , 0 , r , LoadMovie( path_movies , path_movie , tt[ 0 ] ) )
+	frm = Repr( tt , 0 , r , LoadMovie( path_movies , path_movie , tt[ 0 ] ) , master = SpotWindow , cmap = cmap )
 
-	canvas = FigureCanvasTkAgg( frm.figure , master = SpotWindow )  # A tk.DrawingArea.
+	aximg = [ frm.render() ]
 	
-	aximg = frm.render( cmap )
-	
-	canvas.draw()
-	canvas.get_tk_widget().pack( side = tk . TOP , fill=tk.BOTH , expand = 1 )
+	frm.canvas.get_tk_widget().pack( side = tk . TOP , fill=tk.BOTH , expand = 1 )
 #
 	SpotWindow.bind( "<Left>" , lambda event , frame = frm , increment = -1 , aximg = aximg : LeftKey( event , frame , increment , aximg ) )
-	SpotWindow.bind( "<Right>" , lambda event , frame = frm , increment = 1 : aximg = aximg : RightKey( event , frame , increment , aximg ) )
+	SpotWindow.bind( "<Right>" , lambda event , frame = frm , increment = 1 , aximg = aximg : RightKey( event , frame , increment , aximg ) )
 #	#SpotWindow.bind( "<Up>" , lambda event , frame = frm , path = ps , f = f : UpKey( event , tt , path , f ) )
 #	#SpotWindow.bind( "<Down>" , lambda event , tt = tt , path = pr , f = f : DownKey( event , tt , path , f ) )
 ##	SpotWindow.bind( "<BackSpace>" , lambda event , tt = tt , path_sel = ps , path_rej = pr : BackKey( event , tt , path_sel , path_rej ) )
-#	SpotWindow.bind( "-" , lambda event , frame = frm : ZoomOut( event , frame ) )
-#	SpotWindow.bind( "+" , lambda event , frame = frm : ZoomIn( event , frame ) )
+	SpotWindow.bind( "-" , lambda event , frame = frm , increment = 1 , aximg = aximg : ZoomOut( event , frame , increment , aximg ) )
+	SpotWindow.bind( "+" , lambda event , frame = frm , increment = -1 , aximg = aximg : ZoomIn( event , frame , increment , aximg ) )
 #	
 	SpotWindow.mainloop()
