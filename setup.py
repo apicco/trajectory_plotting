44f05bd3e7966f8d933c70e457f3ea0543504cdc
apicco <andrea.picco@unige.ch>
2023-03-07 17:14:35 +0100

    update version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup( name = 'trajectory plotting' ,
-		version = '0.2' ,
+		version = '0.3' ,
 		description = 'Utilities to plot Trajalign trajectories' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
